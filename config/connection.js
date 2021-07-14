01abe108aec51c25b4ad900448c062123b141e85
Mark S <marksherm04@gmail.com>
2021-07-14 15:09:47 -0400

    created User.js and built tables for id, username, password, and email, also filled in index.js to import and export User model data

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 // importing Sequelize constructor from library
 const Sequelize = require('sequelize');
 
+// data in .env will be made available without a variable
 require('dotenv').config();
 
-const sequelize = new Sequelizeq(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// create connection to database, pass in mySQL username and password and hide the info in .env
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 	host: 'localhost',
 	dialect: 'mysql',
 	port: 3306
---
+++ models/User.js
@@ -0,0 +1,57 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create User model
+class User extends Model { }
+
+// table columns
+User.init(
+	{
+		// TABLE COLUMN DEFINTIONS	
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		// define a username column
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		// define an email column
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			// no duplicate emails allowed
+			unique: true,
+			// if allowNull is set to false, we can run our data through validators before creating table data
+			validate: {
+				isEmail: true
+			}
+		},
+		// define a password column
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				// password must be at least 8 characters long
+				len: [8]
+			}
+		}
+	},
+	{
+		// TABLE CONFIGURATION OPTIONS 
+		sequelize,
+		// don't automatically create createdAt/updatedAt timestamp fields
+		timestamps: false,
+		// don't pluralize name of database table
+		freezeTableName: true,
+		// use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+		underscored: true,
+		// make it so our model name stays lowercase in the database
+		modelName: 'user'
+	}
+);
+
+module.exports = User;
