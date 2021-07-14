0b2d57bcb9fe34dfde7547450f1d8008f8c39d09
Mark S <marksherm04@gmail.com>
2021-07-14 15:31:50 -0400

    completed user routes CRUD

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -32,12 +32,61 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
-router.post('/', (req, res) => { });
+router.post('/', (req, res) => { 
+	// expects {username: 'marksherm04', email: 'marksherm04@gmail.com', password: 'primetime!'}
+	User.create({
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password
+	})
+	.then(dbUserData => res.json(dbUserData))
+	.catch(err => {
+		console.log(err);
+		res.status(500).json(err);
+	});
+});
 
-// POST /api/users/1
-router.post('/:id', (req, res) => { });
+// PUT /api/users/1
+router.put('/:id', (req, res) => { 
+	// expects {username: 'marksherm04', email: 'marksherm04@gmail.com', password: 'primetime!'}
+
+	// if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+	User.update(req.body, {
+		where: { 
+			id: req.params.id
+		}
+	})
+	.then(dbUserData => {
+		if (!dbUserData[0]) {
+			res.status(404).json({ message: 'No user found with this id' });
+			return;
+		}
+		res.json(dbUserData);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json(err);
+	});
+});
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => { });
+router.delete('/:id', (req, res) => { 
+	User.destroy({
+		where: {
+			id: req.params.id
+		}
+	})
+	.then(dbUserData => {
+		if (!dbUserData) {
+			res.status(404).json({ message: 'No user found with this id' });
+			return;
+		}
+		res.json(dbUserData);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json(err);
+	});
+});
 
 module.exports = router;
