0a33157ade35bd2891e1c5d1fa2eec8e5f433b40
Mark S <marksherm04@gmail.com>
2021-07-14 14:48:22 -0400

    added files to .gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // importing Sequelize constructor from library
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelizeq('tech_blog_db', 'root', 'Primetime!', {
+require('dotenv').config();
+
+const sequelize = new Sequelizeq(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 	host: 'localhost',
 	dialect: 'mysql',
 	port: 3306
 });
-
-const PORT = process.env.PORT || 3001;
 
 module.exports = sequelize;
