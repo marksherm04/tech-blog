7b709bbb7c075e96863804808cf50c644608a729
Mark S <marksherm04@gmail.com>
2021-07-14 16:49:27 -0400

    updated user-routes and User.js to let the user know if they have the correct email and password when logging in, with bcrypt password if so and add message you are now logged in. else wrong password message will appear

--- models/User.js
+++ models/User.js
@@ -5,7 +5,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create User model
-class User extends Model { }
+class User extends Model { 
+	// set up method to run on instance data per use to check password
+	checkPassword(loginPw) {
+		return bcrypt.compareSync(loginPw, this.password);
+	}
+}
 
 // table columns
 User.init(
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -33,22 +33,46 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
-router.post('/', (req, res) => { 
+router.post('/', (req, res) => {
 	// expects {username: 'john1', email: 'john1@gmail.com', password: 'password!'}
 	User.create({
 		username: req.body.username,
 		email: req.body.email,
 		password: req.body.password
 	})
-	.then(dbUserData => res.json(dbUserData))
-	.catch(err => {
-		console.log(err);
-		res.status(500).json(err);
+		.then(dbUserData => res.json(dbUserData))
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.post('/login', (req, res) => {
+	// Query operation
+	// expects {email: 'john1@gmail.com', password: 'password!'}
+	User.findOne({
+		where: {
+			email: req.body.email
+		}
+	}).then(dbUserData => {
+		if (!dbUserData) {
+			res.status(400).json({ message: 'No user with that email address!' });
+			return;
+		}
+
+		// Verify user
+		const validPassword = dbUserData.checkPassword(req.body.password);
+
+		if (!validPassword) {
+			res.status(400).json({ message: "Incorrect password!" });
+			return;
+		}
+		res.json({ user: dbUserData, message: "You are now logged in!" });
 	});
 });
 
 // PUT /api/users/1
-router.put('/:id', (req, res) => { 
+router.put('/:id', (req, res) => {
 	// expects {'Username: 'john1', email: 'john1@gmail.com', password: 'password!'}
 
 	// if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
@@ -56,35 +80,35 @@ router.put('/:id', (req, res) => {
 		individualHooks: true,
 		where: { id: req.params.id }
 	})
-	.then(dbUserData => {
-		if (!dbUserData[0]) {
-			res.status(404).json({ message: 'No user found with this id' });
-			return;
-		}
-		res.json(dbUserData);
-	})
-	.catch(err => {
-		console.log(err);
-		res.status(500).json(err);
-	});
+		.then(dbUserData => {
+			if (!dbUserData[0]) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => { 
+router.delete('/:id', (req, res) => {
 	User.destroy({
 		where: { id: req.params.id }
 	})
-	.then(dbUserData => {
-		if (!dbUserData) {
-			res.status(404).json({ message: 'No user found with this id' });
-			return;
-		}
-		res.json(dbUserData);
-	})
-	.catch(err => {
-		console.log(err);
-		res.status(500).json(err);
-	});
+		.then(dbUserData => {
+			if (!dbUserData) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
