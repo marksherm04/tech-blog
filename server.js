522114d482173fe49e85a0f805a468d1aa3de001
Mark S <marksherm04@gmail.com>
2021-07-23 16:18:39 -0400

    set up handlebars and added tester info to homepage.handlebars and the html for main.handlebars

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const routes = require('./controllers');
-
+const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const sequelize = require('./config/connection');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(require('./controllers'));
