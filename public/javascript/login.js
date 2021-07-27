83b2a0f7e3b1eb7a8ac100f9d5fccb8d8b9c216a
Mark S <marksherm04@gmail.com>
2021-07-27 14:06:52 -0400

    added login.js and added signup form and login form code

---
+++ public/javascript/login.js
@@ -0,0 +1,52 @@
+async function signupForm(event) {
+	event.preventDefault();
+
+	const username = document.querySelector('#username-signup').value.trim();
+	const email = document.querySelector('#email-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
+
+	if (username && email && password) {
+		const response = await fetch('/api/users', {
+			method: 'post',
+			body: JSON.stringify({
+				username,
+				email,
+				password
+			}),
+			headers: { 'Content-Type': 'application/json' }
+		});
+		// response status check
+		if (response.ok) {
+			console.log('success');
+		} else {
+			alert(response.statusText);
+		};
+	};
+};
+
+async function loginForm(event) {
+	event.preventDefault();
+      
+	const email = document.querySelector('#email-login').value.trim();
+	const password = document.querySelector('#password-login').value.trim();
+      
+	if (email && password) {
+	  const response = await fetch('/api/users/login', {
+	    method: 'post',
+	    body: JSON.stringify({
+	      email,
+	      password
+	    }),
+	    headers: { 'Content-Type': 'application/json' }
+	  });
+   
+	  if (response.ok) {
+	    document.location.replace('/');
+	  } else {
+	    alert(response.statusText);
+	  }
+	}
+      }
+      
+      document.querySelector('.signup-form').addEventListener('submit', signupForm);
+      document.querySelector('.login-form').addEventListener('submit', loginForm);
