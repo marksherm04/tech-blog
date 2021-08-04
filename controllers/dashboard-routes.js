6127f236900a9876da37f4c0e921d0700276527a
Mark S <marksherm04@gmail.com>
2021-08-04 15:23:23 -0400

    updated dashboard and homepage to show edit and post content

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -33,7 +33,7 @@ router.get('/', withAuth, (req, res) => {
 		.then(dbPostData => {
 			// serialize data before passing to template
 			const posts = dbPostData.map(post => post.get({ plain: true }));
-			res.render('dashboard', { loggedIn: true });
+			res.render('dashboard', { loggedIn: true, posts });
 		})
 		.catch(err => {
 			console.log(err);
